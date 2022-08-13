35a654410a222b61840671fcca69bc2456304ddb
Savio Picanço <saviopbrito@outlook.com>
2022-08-13 16:06:02 -0300

    feat: ensure SignUpController return an error if no name provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,7 +2,8 @@ export class SingUpController {
     handle(httpRequest : any ) : any{
 
         return {
-            statusCode : 400
+            statusCode : 400,
+            body: new Error('Missing param: name')
         }
     }
 }
