efb3d9da35aed82b40cdffe7c040f1a9d04bf71c
Savio Picanço <saviopbrito@outlook.com>
2022-08-13 16:06:34 -0300

    test: ensure SignUpController return an error if no name provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -15,5 +15,6 @@ describe('Sing up Controller', () => {
     const httpResponse = sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
