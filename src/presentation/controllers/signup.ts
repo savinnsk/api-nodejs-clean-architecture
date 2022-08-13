d188d8f6793c9a7569cbf271aa9e1014e6153af6
Savio Picanço <saviopbrito@outlook.com>
2022-08-13 17:28:13 -0300

    refactor: simplify code signup.ts

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,12 +4,12 @@ import { badRequest } from "../helpers/http-helper"
 
 export class SingUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamsError('name'))
-    }
+    const requiredFields = ['name', 'email']
 
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamsError('email'))
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamsError(field))
+      }
     }
   }
 }
