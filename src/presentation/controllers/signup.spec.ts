091a43b05f4cc4e3ba4a58e48f429dd717068458
Savio Picanço <saviopbrito@outlook.com>
2022-08-13 15:51:22 -0300

    test: ensure SignupController return 400 if no name provider

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,19 @@
+import {SingUpController} from './signup'
+
+describe('Sing up Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SingUpController()
+
+    const httpRequest = {
+      body: {
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
