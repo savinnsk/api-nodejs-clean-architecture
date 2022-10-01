cadb79d15fc18b94d8fa209760046e4c466bdacb
Savio Picanço <saviopbrito@outlook.com>
2022-09-30 22:04:28 -0300

    chore: add jest configs and scripts

---
+++ jest-unity-config.js
@@ -0,0 +1,7 @@
+/* eslint-disable semi */
+/* eslint-disable quotes */
+const config = require("./jest.config");
+
+config.testMatch = ["**/*.spec.ts"];
+
+module.exports = { config };
---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable semi */
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+module.exports = {
+  clearMocks: true,
+  roots: ["<rootDir>/src"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  coverageProvider: "v8",
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/*.spec.ts"],
+  transform: {
+    ".+\\ts$": "ts-jest",
+  },
+};
