76f6064f789b207f6137b493d403d7bdb88f3d1b
Savio Picanço <saviopbrito@outlook.com>
2022-10-11 21:59:57 -0300

    refactor: send content

--- src/main/middlewares/body-parser/body-parse.test.ts
+++ src/main/middlewares/body-parser/body-parse.test.ts
@@ -3,10 +3,12 @@ import app from '../../config/app'
 
 describe("Body parse Middleware", () => {
   test('Should parser body as json', async () => {
-    app.post('/test_body_parse', (req, res) => {
+    app.post('/test_body_parser', (req, res) => {
       res.send(req.body)
     })
     await request(app)
       .post('/test_body_parser')
+      .send({ name: 'name_test' })
+      .expect({ name: 'name_test' })
   })
 })
