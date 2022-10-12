1a6f99ffe5a5d2a4be502ba48a04adb6c6716bdb
Savio Picanço <saviopbrito@outlook.com>
2022-10-12 12:18:55 -0300

    refactor: group middlewares interfaces in a sigle file

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,5 @@
 import { Express } from "express"
-import { bodyParser } from "../middlewares/body-parser/body-parser"
-import { contentType } from "../middlewares/content-type/content-type"
-import { cors } from "../middlewares/cors/cors"
+import { bodyParser, cors, contentType } from "../middlewares/index"
 
 export default (app: Express): void => {
   app.use(bodyParser)
