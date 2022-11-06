c95c870168eefa8c8c9165d8c7d106ae5722bdf1
Savio Picanço <saviopbrito@outlook.com>
2022-11-06 18:33:28 -0300

    should return 400 if no email is provider

---
+++ src/presentation/controllers/login/login.ts
@@ -0,0 +1,14 @@
+
+import { MissingParamsError } from "../../errors"
+import { badRequest } from "../../helpers/http-helper"
+import { Controller, HttpRequest, HttpResponse } from "../../protocols"
+
+export class LoginController implements Controller {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      return new Promise(resolve => resolve(badRequest(new MissingParamsError("email"))))
+    } catch (err) {
+
+    }
+  }
+}
