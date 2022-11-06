64f9ea23e13d56fa5ac864cc8700563d307fde11
Savio Picanço <saviopbrito@outlook.com>
2022-11-06 18:34:14 -0300

    test: should return 400 if no email is provider

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,23 @@
+import { MissingParamsError } from "../../errors"
+import { badRequest } from "../../helpers/http-helper"
+// import { HttpRequest } from "../../protocols"
+import { LoginController } from "./login"
+
+/* const makeFakeHttpRequest = (): HttpRequest => ({
+  body: {
+    email: "any_mail@mail.com",
+    password: "any_password"
+  }
+}) */
+
+const makeSut = (): LoginController => {
+  return new LoginController()
+}
+
+describe("Login Controller", () => {
+  test("should return 400 if no email is provider", async () => {
+    const sut = makeSut()
+    const httpResponse = await sut.handle({ body: { password: "any_password" } })
+    expect(httpResponse).toEqual(badRequest(new MissingParamsError("email")))
+  })
+})
