f51608070d44b3648595b1004667fe261816075b
Savio Picanço <saviopbrito@outlook.com>
2022-11-10 14:02:35 -0300

    test: enusre LoginController return 500 if authentication throws

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -97,4 +97,11 @@ describe("Login Controller", () => {
     const httpResponse = await sut.handle(makeFakeHttpRequest())
     expect(httpResponse).toEqual(unauthorized())
   })
+
+  test("should return 500 if Authentication throws", async () => {
+    const { sut, authenticationStub } = makeSut()
+    jest.spyOn(authenticationStub, "auth").mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const httpResponse = await sut.handle(makeFakeHttpRequest())
+    expect(httpResponse).toEqual(serverError(new ServerError(null)))
+  })
 })
