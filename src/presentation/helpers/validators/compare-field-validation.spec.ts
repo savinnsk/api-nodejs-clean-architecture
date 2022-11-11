9335107fd6a3b4c1149fa3c8170c97b3a20249f9
Savio Picanço <saviopbrito@outlook.com>
2022-11-11 12:43:51 -0300

    test:  add tests to CompareFieldsValidation

---
+++ src/presentation/helpers/validators/compare-field-validation.spec.ts
@@ -0,0 +1,20 @@
+import { InvalidParamsError } from "../../errors"
+import { CompareFieldsValidation } from "./compare-field-validation"
+
+const makeSut = (): CompareFieldsValidation => {
+  return new CompareFieldsValidation("field", "fieldToCompare")
+}
+
+describe("CompareField Validation", () => {
+  test("Should return a InvalidParamsError if validator throws", () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: "any_value", fieldToCompare: "wrong_compare" })
+    expect(error).toEqual(new InvalidParamsError("fieldToCompare"))
+  })
+
+  test("should not return if validation success", () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: "any_value", fieldToCompare: "any_value" })
+    expect(error).toBeFalsy()
+  })
+})
--- src/presentation/helpers/validators/compare-field-validation.ts
+++ src/presentation/helpers/validators/compare-field-validation.ts
@@ -12,7 +12,7 @@ export class CompareFieldsValidation implements Validation {
 
   validate (input: any): Error {
     if (input[this.fieldName] !== input[this.fieldToCompareName]) {
-      return new InvalidParamsError(input[this.fieldToCompareName])
+      return new InvalidParamsError(this.fieldToCompareName)
     }
   }
 }
