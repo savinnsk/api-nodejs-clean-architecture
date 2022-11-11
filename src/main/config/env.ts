ee7d9dc4e6421986310b4dce641c55c4bbb8034a
Savio Picanço <saviopbrito@outlook.com>
2022-11-11 11:51:44 -0300

    config: add apiCluster

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,8 @@
+const username = "savinnsk"
+const password = "EGA130pha100"
+
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/clean-code-api',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  mongoAPiCluster: `mongodb+srv://${username}:${password}@apicluster0.u3gd8.mongodb.net/?retryWrites=true&w=majority`
 }
