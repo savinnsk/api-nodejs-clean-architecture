db91aac6e85a5e9384a1ccad99e490581215ab0f
Savio Picanço <saviopbrito@outlook.com>
2022-11-12 13:22:29 -0300

    refactor: ensure ValidationComposite not return if validation success

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -40,4 +40,10 @@ describe("Validation Composite", () => {
     const error = sut.validate({ field: "any_value" })
     expect(error).toEqual(new Error())
   })
+
+  test("Should not return if validation success ", () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: "any_value" })
+    expect(error).toBeFalsy()
+  })
 })
