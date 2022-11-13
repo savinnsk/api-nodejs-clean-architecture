f0dfee854a0c8a3358729ccffc906f9c173799cf
Savio Picanço <saviopbrito@outlook.com>
2022-11-13 15:47:07 -0300

    feat: ensures calls loadAccountByEmailRepository with correct email

---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,15 @@
+import { Authentication, AuthenticationDTO } from "../../../domain/usecases/authentication"
+import { LoadAccountByEmailRepository } from "../../protocols/load-account-email-repository"
+
+export class DbAuthentication implements Authentication {
+  private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository) {
+    this.loadAccountByEmailRepository = loadAccountByEmailRepository
+  }
+
+  async auth (authentication: AuthenticationDTO): Promise<string> {
+    const { email } = authentication
+    await this.loadAccountByEmailRepository.load(email)
+    return null
+  }
+}
