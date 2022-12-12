b4616bbf83a6b8a75b2e1fe675beb8ab01e27747
Savio Picanço <saviopbrito@outlook.com>
2022-12-12 18:49:03 -0300

    refactor: move bcrypt adapter-files to a folder

--- src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.spec.ts
@@ -44,4 +44,16 @@ describe('Bcrypt Adapter', () => {
     const isValid = await sut.compare("any_value", "any_hash")
     expect(isValid).toBe(true)
   })
+
+  /*  test('Should return false when compare fails', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'compare').mockReturnValueOnce(new Promise(resolve => resolve(false)))
+    const isValid = await sut.compare("any_value", "any_hash")
+    expect(isValid).toBe(false)
+  }) */
+
+  /* test("Should throw if compare throws", () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'compare').mockReturnValueOnce(new Promise(reject => reject(new Error())))
+  }) */
 })
--- src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -1,6 +1,6 @@
-import { Hasher } from "../../data/protocols/cryptography/hasher"
+import { Hasher } from "../../../data/protocols/cryptography/hasher"
 import bcrypt from 'bcrypt'
-import { HashComparer } from "../../data/protocols/cryptography/hash-compare"
+import { HashComparer } from "../../../data/protocols/cryptography/hash-compare"
 
 export class BcryptAdapter implements Hasher, HashComparer {
   private readonly salt: number
