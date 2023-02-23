52aa6ee5dfde83bfe32f3b07b4a6bcd4cad48e1b
savinnsk <savio.document@gmail.com>
2023-02-22 23:24:23 -0300

    feat: add load by email to repository method

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,10 +1,27 @@
-import { AddAccountRepository, AccountModel, AddAccountModelDTO, MongoHelper } from "./account-protocols"
+import { LoadAccountByEmailRepository } from "../../../../data/protocols/db/load-account-email-repository";
+import {
+  AddAccountRepository,
+  AccountModel,
+  AddAccountModelDTO,
+  MongoHelper,
+} from "./account-protocols";
 
-export class AccountMongoRepository implements AddAccountRepository {
-  async add (accountData: AddAccountModelDTO): Promise<AccountModel> {
-    const accountCollection = await MongoHelper.getCollection('accounts')
-    const createAccount = await accountCollection.insertOne(accountData)
-    const account = await accountCollection.findOne({ _id: createAccount.insertedId })
-    return MongoHelper.map(account)
+export class AccountMongoRepository
+  implements AddAccountRepository, LoadAccountByEmailRepository
+{
+  async add(accountData: AddAccountModelDTO): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection("accounts");
+    const createAccount = await accountCollection.insertOne(accountData);
+    const account = await accountCollection.findOne({
+      _id: createAccount.insertedId,
+    });
+    return MongoHelper.map(account);
+  }
+
+  async loadByEmail(email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection("accounts");
+    const account = await accountCollection.findOne({ email: email });
+
+    return MongoHelper.map(account);
   }
 }
