bf3aac8c454e94d23922b58eb9588c8b912bfb6f
savinnsk <savio.document@gmail.com>
2023-02-22 23:35:46 -0300

    feat: update access token by id

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,4 +1,5 @@
 import { LoadAccountByEmailRepository } from "../../../../data/protocols/db/load-account-email-repository";
+import { UpdateAccessTokenRepository } from "../../../../data/protocols/db/update-access-token-repository";
 import {
   AddAccountRepository,
   AccountModel,
@@ -7,7 +8,10 @@ import {
 } from "./account-protocols";
 
 export class AccountMongoRepository
-  implements AddAccountRepository, LoadAccountByEmailRepository
+  implements
+    AddAccountRepository,
+    LoadAccountByEmailRepository,
+    UpdateAccessTokenRepository
 {
   async add(accountData: AddAccountModelDTO): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection("accounts");
@@ -23,5 +27,19 @@ export class AccountMongoRepository
     const account = await accountCollection.findOne({ email: email });
 
     return MongoHelper.map(account);
+  }
+
+  async updateAccessToken(id: string, token: string): Promise<void> {
+    const accountCollection = await MongoHelper.getCollection("accounts");
+    await accountCollection.updateOne(
+      {
+        _id: id as any,
+      },
+      {
+        $set: {
+          accessToken: token,
+        },
+      }
+    );
   }
 }
