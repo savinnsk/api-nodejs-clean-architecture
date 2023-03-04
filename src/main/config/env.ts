3257261885f942bc643098d5cce78c2bedef514f
savinnsk <savio.document@gmail.com>
2023-03-04 12:09:13 -0300

    feat: add loginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 export default {
   mongoUrl: process.env.MONGO_URL || "mongodb://127.0.0.1:27017/",
   port: process.env.PORT || 5050,
-
+  secret: process.env.JWT_SECRET || "random123",
   mongoAPiCluster: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@savinnsk.bmso1rh.mongodb.net/?retryWrites=true&w=majority`,
 };
