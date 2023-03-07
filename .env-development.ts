f90e7ab47768692124e872348a05d851cd229426
savinnsk <savio.document@gmail.com>
2023-03-07 18:12:15 -0300

    chore: dotenv config

--- .env-development.ts
+++
@@ -1,4 +0,0 @@
-export const envDev = {
-  password: "7anC8Rvcp8UQt6JL",
-  user: "savio",
-};
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,9 +1,9 @@
-import "dotenv";
-import { envDev } from "../../../.env-development";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export default {
   mongoUrl: process.env.MONGO_URL || "mongodb://127.0.0.1:27017/",
   port: process.env.PORT || 5050,
   secret: process.env.JWT_SECRET || "random123",
-  mongoAPiCluster: `mongodb+srv://${envDev.user}:${envDev.password}@cluster0.frion87.mongodb.net/?retryWrites=true&w=majority`,
+  mongoAPiCluster: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.frion87.mongodb.net/?retryWrites=true&w=majority`,
 };
