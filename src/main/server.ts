d907e7c5cd9d1823215d6aa5a0ea1e7b7cabf3a5
savinnsk <savio.document@gmail.com>
2023-03-09 17:05:59 -0300

    add souce map to debug code

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,9 @@
 import { MongoHelper } from "../infra/db/mongodb/helpers/mongodb-helper";
 import env from "./config/env";
 
-MongoHelper.connect(env.mongoUrl)
+import "source-map-support/register";
+
+MongoHelper.connect(env.mongoAPiCluster)
   .then(async () => {
     const app = (await import("./config/app")).default;
     app.listen(env.port, () => console.log(`server running at ${env.port}`));
