47a7a1e13e82de4613d6c6719c9f9d8d356f66df
savinnsk <savio.document@gmail.com>
2023-04-17 20:00:01 -0300

    test: Should return 200 with a accessToken if valid data is provided

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -113,10 +113,10 @@ describe("Sing up Controller", () => {
     });
   });
 
-  test("Should return 200 if valid data is provided", async () => {
+  test("Should return 200 with a accessToken if valid data is provided", async () => {
     const { sut } = makeSut();
     const httpResponse = await sut.handle(makeFakeRequest());
-    expect(httpResponse).toEqual(ok(makeFakeAccount()));
+    expect(httpResponse).toEqual(ok({ accessToken: "any_token" }));
   });
 
   test("Should call Validator with correct values", async () => {
