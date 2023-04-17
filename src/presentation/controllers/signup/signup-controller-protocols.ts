0f8b3f6722b75521182c6b7b75eae2f1d70532ec
savinnsk <savio.document@gmail.com>
2023-04-17 17:16:06 -0300

    feat:should call Authentication with correct value

--- src/presentation/controllers/signup/signup-controller-protocols.ts
+++ src/presentation/controllers/signup/signup-controller-protocols.ts
@@ -1,5 +1,13 @@
-export * from "../../protocols";
+// validations
 export * from "../../protocols/email-validator";
-export * from "../../../domain/usecases/add-account";
-export * from "../../../domain/models/account";
 export * from "../../protocols/validation-helper";
+
+//usecases
+export * from "../../../domain/usecases/add-account";
+export * from "../../../domain/usecases/authentication";
+
+// models & dto
+export * from "../../../domain/models/account";
+
+// utils interfaces
+export * from "../../protocols";
--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -4,13 +4,15 @@ import {
   HttpResponse,
   AddAccount,
   Validation,
+  Authentication,
 } from "./signup-controller-protocols";
 import { badRequest, serverError, ok } from "../../helpers/http/http-helper";
 
 export class SignUpController implements Controller {
   constructor(
     private readonly addAccount: AddAccount,
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly authentication: Authentication
   ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -27,6 +29,12 @@ export class SignUpController implements Controller {
         email,
         password,
       });
+
+      await this.authentication.auth({
+        email: account.email,
+        password: account.password,
+      });
+
       return ok(account);
     } catch (error) {
       return serverError(error);
