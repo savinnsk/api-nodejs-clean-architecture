87617c3beb0c62ffc1ffb56a2396468a578f6339
savinnsk <savio.document@gmail.com>
2023-04-17 21:33:42 -0300

    feat: add error forbiden and email already in use

--- src/presentation/helpers/http/http-helper.ts
+++ src/presentation/helpers/http/http-helper.ts
@@ -1,25 +1,28 @@
-import { HttpResponse } from "../../protocols/http"
-import { ServerError } from "../../errors"
-import { Unauthorized } from "../../errors/unauthorized-error"
+import { HttpResponse } from "../../protocols/http";
+import { ServerError } from "../../errors";
+import { Unauthorized } from "../../errors/unauthorized-error";
 
-export const badRequest = (error: Error): HttpResponse => (
-  {
-    statusCode: 400,
-    body: error
-  }
-)
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error,
+});
+
+export const forbidden = (error: Error): HttpResponse => ({
+  statusCode: 403,
+  body: error,
+});
 
 export const serverError = (error: Error): HttpResponse => ({
   statusCode: 500,
-  body: new ServerError(error.stack)
-})
+  body: new ServerError(error.stack),
+});
 
 export const unauthorized = (): HttpResponse => ({
   statusCode: 401,
-  body: new Unauthorized()
-})
+  body: new Unauthorized(),
+});
 
 export const ok = (data): HttpResponse => ({
   statusCode: 200,
-  body: data
-})
+  body: data,
+});
