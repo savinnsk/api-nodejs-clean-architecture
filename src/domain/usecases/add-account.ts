96cbe8a358af347923bad09e5e4d762134b2cc99
savinnsk <savio.document@gmail.com>
2023-04-17 21:34:31 -0300

    refactor: add posible null response to add account

--- src/domain/usecases/add-account.ts
+++ src/domain/usecases/add-account.ts
@@ -1,11 +1,11 @@
-import { AccountModel } from "../models/account"
+import { AccountModel } from "../models/account";
 
 export interface AddAccountModelDTO {
-  name: string
-  email: string
-  password: string
+  name: string;
+  email: string;
+  password: string;
 }
 
-export interface AddAccount{
-  add: (account: AddAccountModelDTO) => Promise<AccountModel>
+export interface AddAccount {
+  add: (account: AddAccountModelDTO) => Promise<AccountModel | null>;
 }
