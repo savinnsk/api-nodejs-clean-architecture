8e95dea4025f9ffb2fd2f86a1936d1253ffe847e
savinnsk <savio.document@gmail.com>
2023-04-29 17:04:53 -0300

    test: ensure AddSurveyUseCase throws if AddSurveyRepository throws

--- src/data/usecases/add-survey/add-survey.spec.ts
+++ src/data/usecases/add-survey/add-survey.spec.ts
@@ -1,6 +1,8 @@
 import { AddSurveyModel } from "@/domain/usecases/survey/add-survey";
 import { AddSurveyUseCase } from "./add-survey-usecase";
 import { AddSurveyRepository } from "@/data/protocols/survey/add-survey-repository";
+import { serverError } from "@/presentation/helpers/http/http-helper";
+import { ServerError } from "@/presentation/errors";
 
 type SutType = {
   sut: AddSurveyUseCase;
@@ -19,7 +21,7 @@ const fakeRequest: AddSurveyModel = {
 
 const makeAddSurveyRepositoryStub = (): AddSurveyRepository => {
   class AddSurveyRepositoryStub implements AddSurveyRepository {
-    async add(surveyData: AddSurveyModel): Promise<void> {}
+    async add(surveyData: AddSurveyModel): Promise<void | Error> {}
   }
 
   return new AddSurveyRepositoryStub();
@@ -35,12 +37,12 @@ const makeSut = (): SutType => {
 };
 
 describe("AddSurveyUseCase", () => {
-  test("should calls AddSurveyRepository with correct values", () => {
+  test("should calls AddSurveyRepository with correct values", async () => {
     const { sut, addSurveyRepositoryStub } = makeSut();
 
     const spy = jest.spyOn(addSurveyRepositoryStub, "add");
 
-    sut.add(fakeRequest);
+    await sut.add(fakeRequest);
 
     expect(spy).toHaveBeenCalledWith({
       question: "any_question",
@@ -51,5 +53,19 @@ describe("AddSurveyUseCase", () => {
         },
       ],
     });
+  });
+
+  test("should throws if AddSurveyRepository throws", async () => {
+    const { sut, addSurveyRepositoryStub } = makeSut();
+
+    jest
+      .spyOn(addSurveyRepositoryStub, "add")
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+
+    const promise = sut.add(fakeRequest);
+
+    await expect(promise).rejects.toThrow();
   });
 });
