30f5a0de6789e8b50efca446454374c267acebac
savinnsk <savio.document@gmail.com>
2023-05-08 16:40:44 -0300

    feat :ensure AuthMidleware call LoadAccountByToken with correct values

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -5,14 +5,20 @@ import { forbidden, ok, serverError } from "../helpers/http/http-helper";
 import { AccessDeniedError } from "../errors/access-denied-error";
 
 export class AuthMiddleware implements Middleware {
-  constructor(private readonly loadAccountByToken: LoadAccountByToken) {}
+  constructor(
+    private readonly loadAccountByToken: LoadAccountByToken,
+    private readonly role?: string
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const accessToken = httpRequest.headers?.["x-access-token"];
 
       if (accessToken) {
-        const account = await this.loadAccountByToken.load(accessToken);
+        const account = await this.loadAccountByToken.load(
+          accessToken,
+          this.role
+        );
 
         if (account) {
           return ok({ accountId: account.id });
